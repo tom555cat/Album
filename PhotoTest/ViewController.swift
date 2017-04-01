3ba2ad685de3332108e199b4081f368131df37c4
BJTU-HT <bjtu-ht@163.com>
2017-04-01 08:57:46 +0800

    Initial Commit

---
+++ PhotoTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoTest
+//
+//  Created by tom555cat on 2017/4/1.
+//  Copyright © 2017年 Hello World Corporation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
